94cbbaebfdea862c93e1586ccfebe7b52bb4d674
avlonger <alexander.loptev@gmail.com>
2015-01-23 01:56:40 +0300

    test some hash functions

--- src/etc.cpp
+++ src/etc.cpp
@@ -1,9 +1,32 @@
 #include "etc.h"
 
+#include <array>
+
 int highest_bit(int n) {
     int r = 0;
     while (n >>= 1) {
         ++r;
     }
     return r;
+}
+
+namespace std
+{
+    template<typename T, size_t N>
+    struct hash<array<T, N> >
+{
+    typedef array<T, N> argument_type;
+    typedef size_t result_type;
+
+    result_type operator()(const argument_type& a) const
+    {
+        hash<T> hasher;
+        result_type h = 0;
+        for (result_type i = 0; i < N; ++i)
+        {
+            h = h * 4093 + hasher(a[i]);
+        }
+        return h;
+    }
+};
 }
--- src/etc.h
+++ src/etc.h
@@ -1,6 +1,11 @@
 #ifndef PERIODLIB_ETC_H_
 #define PERIODLIB_ETC_H_
 
+#include <array>
+
 int highest_bit(int n);
+
+template<typename T, size_t N>
+struct hash<std::array<T, N> >;
 
 #endif  // PERIODLIB_ETC_H_
